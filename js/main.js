e0108f3ac88f1f773218130ab7e4c90cd7e37719
Athyuttam Reddy <athyuttamre@gmail.com>
2016-03-24 13:25:34 -0400

    Add more instructions videos, transitions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
 $(document).ready(function() {
+  // Instructions
+  function playInstructionsVideo() {
+    var $screenshotVid = $(".screenshot-vid");
+    var $drawingVid = $(".drawing-vid");
+    var $basketballVid = $(".basketball-vid");
+
+    setTimeout(function() {
+      $screenshotVid[0].play();
+      setTimeout(function() {
+        $screenshotVid.fadeOut();
+        $drawingVid[0].play();
+        setTimeout(function() {
+          $drawingVid.fadeOut();
+          $basketballVid[0].play();
+        }, 12000);
+      }, 3000);
+    }, 1000);
+  }
+
+  $(window).scroll(function() {
+    var hT = $('.instructions').offset().top,
+       hH = $('.instructions').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+   if (wS > (hT+hH-wH)){
+       console.log('you have scrolled to instructions');
+       playInstructionsVideo();
+   }
+  });
+
+  // Examples
   var $examplePhones = $(".example-phone");
   $examplePhones.hover(function() {
     $examplePhones.removeClass("active");
